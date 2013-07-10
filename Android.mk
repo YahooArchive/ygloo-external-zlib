0b443f50162159fe2692c15ec357354c19ddc88d
Eric Hassold <hassold@yahoo-inc.com>
2013-07-10 11:08:14 -0700

    Add makefile (named Android.mk but supports all target platforms)
    Change-Id: I06aef7cf20749ba53254a9e17d704f22b98ab565

---
+++ Android.mk
@@ -0,0 +1,37 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+ZLIB_SRC_FILES := \
+	adler32.c \
+	compress.c \
+	crc32.c \
+	deflate.c \
+	gzclose.c \
+	gzlib.c \
+	gzread.c \
+	gzwrite.c \
+	infback.c \
+	inflate.c \
+	inftrees.c \
+	inffast.c \
+	trees.c \
+	uncompr.c \
+	zutil.c
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libyahoo_zlib
+LOCAL_MODULE_TAGS := optional
+LOCAL_ARM_MODE := arm
+LOCAL_CFLAGS += -O3 -DUSE_MMAP
+LOCAL_SRC_FILES := $(ZLIB_SRC_FILES)
+ifeq ($(TARGET_ARCH),arm)
+  LOCAL_NDK_VERSION := 5
+  LOCAL_SDK_VERSION := 9
+endif
+
+# Force compiling the static library as PIC so it can be embedded
+# into a shared library later
+LOCAL_CFLAGS += -fPIC -DPIC
+
+include $(BUILD_STATIC_LIBRARY)
